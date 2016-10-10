69e6b8371b287983ce8cf8572f99b7e968b8ba6b
kunwe <1508974740@qq.com>
2016-10-10 14:31:14 +0800

    Initial commit

---
+++ new5.02/src/main/java/com/kunkun/new50/MainActivity.java
@@ -0,0 +1,23 @@
+package com.kunkun.new50;
+
+import android.app.Activity;
+import android.graphics.Outline;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewOutlineProvider;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.tv).setOutlineProvider(new ViewOutlineProvider() {
+            @Override
+            public void getOutline(View view, Outline outline) {
+                outline.setOval(0, 0, view.getWidth() + 10, view.getHeight() + 10);
+            }
+        });
+    }
+}
